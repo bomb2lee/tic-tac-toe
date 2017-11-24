259ee4e53eaf92e1ed6274d8eaaf86c795eb0229
bomb2lee <33406153+bomb2lee@users.noreply.github.com>
2017-11-24 22:15:17 +0900

    60162123 victory condition

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -15,6 +15,20 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
+      
+      row = i//3
+      col = i%3
+      
+      if list[row*3]["text"] == list[row*3+1]["text"] == list[row*3+2]["text"] :
+            winner(row)
+      elif list[col]["text"] == list[col+3]["text"] == list[col+6]["text"] :
+            winner(col)
+      elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     " or list[2]["text"] == list[4]["text"] == list[6]["text"] !="     " :
+            winner(4)
+
+def winner(i) :
+      msg = Label(window, text = "winner is " + list[i]["text"])
+      msg.grid(row=3, column=1)
 
 window = Tk()
 player = "X"
